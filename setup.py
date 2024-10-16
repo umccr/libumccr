741d96e4e780f291e3e2c63663c346d35ce098de
Victor San Kho Lin <victor@sankholin.com>
2024-10-16 20:17:13 +1100

    Release 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="libumccr",
-    version="0.4.0rc4",
+    version="0.4.0",
     author="UMCCR and Contributors",
     author_email="services@umccr.org",
     description="UMCCR Reusable Python modules",
